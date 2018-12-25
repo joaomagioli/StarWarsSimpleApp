86c106c8030d91f22c99521147c1c5bbc7a12079
Joao Guilherme <joaomagioli@gmail.com>
2018-12-25 12:23:09 -0200

    Api class created

---
+++ star_wars_app/lib/api/api.dart
@@ -0,0 +1,24 @@
+import 'package:star_wars_app/wrapper/movies_wrapper.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+class Api {
+  static final String _baseUrl = "https://swapi.co/api/";
+
+  // Method that consumes the endpoint
+  Future<MoviesWrapper> fetchMovies(http.Client client) async {
+    final response = await client.get(_baseUrl + 'films');
+    if (response.statusCode == 200) {
+      return _parseMovies(response.body);
+    } else {
+      throw Exception('Error fetching movies');
+    }
+  }
+
+  // Method that parses the response
+  MoviesWrapper _parseMovies(String responseBody) {
+    final parsed = json.decode(responseBody);
+    return MoviesWrapper.fromJson(parsed);
+  }
+}
--- star_wars_app/lib/wrapper/movies_wrapper.dart
+++ star_wars_app/lib/wrapper/movies_wrapper.dart
@@ -13,6 +13,9 @@ class MoviesWrapper {
   factory MoviesWrapper.fromJson(Map<String, dynamic> json) {
     var list = json['results'] as List;
 
+    // We are receiving a list of dynamics, but moviesResults expects a list of movies
+    // So we have to convert to the type
+    // We are mapping (transforming) the dynamic to Movie object
     List<Movie> moviesList = list.map((item) => Movie.fromJson(item)).toList();
 
     return MoviesWrapper(
